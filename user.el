57cad6b161c529c2e820ab7be3976b7b9a94ee7e
Roberto Acevedo <rxacevedo@gmail.com>
2014-04-30 08:20:34 -0400

    org-mode sync

--- user.el
+++ user.el
@@ -38,6 +38,13 @@
 (load-theme 'tomorrow-night-bright t)
 
 ;; Org-mode
+
+;; Set to the location of your Org files on your local system
+(setq org-directory "~/org")
+;; Set to the name of the file where new notes will be stored
+(setq org-mobile-inbox-for-pull "~/org/flagged.org")
+;; Set to <your Dropbox root directory>/MobileOrg.
+(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
