af345f3d97b1b1c447777eecc1cd1f910f0b3f58
Roberto Acevedo <rxacevedo@gmail.com>
2014-08-10 00:36:19 +0100

    Only load path when not on Windows

--- init.el
+++ init.el
@@ -1,15 +1,16 @@
 (require 'package)
 
 ;; Fix path for Emacs gui
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
-
-(if window-system (set-exec-path-from-shell-PATH))
+;; Don't need to set path for the Windows GUI client
+(when (not (and window-system (eq 'windows-nt system-type)))
+  (defun set-exec-path-from-shell-PATH ()
+    (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+      (setenv "PATH" path-from-shell)
+      (setq exec-path (split-string path-from-shell path-separator))))
+  (set-exec-path-from-shell-PATH))
 
 (set-face-attribute 'default nil :font "Source Code Pro-14" )
-(set-frame-font "Source Code Pro-14"  nil t)
+(set-frame-font "Source Code Pro Medium-11"  nil t)
 
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
