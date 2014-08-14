ea6fd72ef9cc7bbefc643825790514a9b2a8616e
Roberto Acevedo <rxacevedo@gmail.com>
2014-08-13 20:06:02 -0400

    inbox makes more sense

--- user.el
+++ user.el
@@ -89,7 +89,7 @@
 
 ;; Capture templates
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/org/gtd.org" "Tasks")
+      '(("t" "Todo" entry (file+headline "~/org/inbox.org" "Tasks")
              "* TODO %?\n  %i\n  %a")
         ("j" "Journal" entry (file+datetree "~/org/journal.org")
          "* %?\nEntered on %U\n  %i\n  %a")
